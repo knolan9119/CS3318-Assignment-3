5c9a431cdb081039489c25eabce8ca81f779caeb
Killian <knolan9119@gmail.com>
2021-11-30 21:10:54 +0000

    Added User Class which has an emailAddress and Password. Added test code for functionality of user class.

---
+++ src/main/java/cs3318assignment3tdd/cs3318javatdd/User.java
@@ -0,0 +1,32 @@
+package cs3318assignment3tdd.cs3318javatdd;
+
+public class User {
+    private EmailAddress emailAddress;
+    private Password password;
+
+    public User(final EmailAddress emailAddress, final Password password) {
+            this.emailAddress = emailAddress;
+            this.password = password;
+    }
+
+    public String getPasswordAsString() {
+        return this.password.getPassword();
+    }
+
+    public String getEmailAddressAsString() {
+        return this.emailAddress.getEmailAddress();
+    }
+
+    public void setPassword(Password password) {
+        this.password = password;
+    }
+
+    public void setEmailAddress(EmailAddress emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "email address: "+this.getEmailAddressAsString()+"\npassword: "+this.getPasswordAsString();
+    }
+}

9a2ce192e1f0f84841842ccff50c2809a8c882dd
Killian <knolan9119@gmail.com>
2021-12-01 17:18:19 +0000

    -Implemented functionality of a user being created and added to a user list if a valid email address and password are entered and the submit button is hit. -The user is given a visual prompt if the details are entered incorrectly or if they have succeeded in registering. -Changes to method in SignUpController: Submit

--- src/main/java/cs3318assignment3tdd/cs3318javatdd/SignUpController.java
+++ src/main/java/cs3318assignment3tdd/cs3318javatdd/SignUpController.java
@@ -20,10 +20,42 @@ public class SignUpController {
     private String email;
     private String password;
 
+    private EmailAddress validEmailAddress;
+    private Password validPassword;
+    private User newUser;
+
+    private UserList listOfUsers = new UserList();
+
     @FXML
     private void submit(ActionEvent event) {
         email = userEmailAddress.getText();
         password = userPassword.getText();
+        try{
+            validEmailAddress = new EmailAddress(email);
+            validPassword = new Password(password);
+            newUser = new User(validEmailAddress,validPassword);
+            listOfUsers.addUser(newUser);
+            messageField.setText("Thank You for Signing Up!");
+        }
+        catch(IllegalArgumentException e) {
+            messageField.setText("Please enter a valid email address and password.");
+        }
+        catch(DuplicateUserException e) {
+            messageField.setText("A user is already registered with this email address.");
+        }
+        catch(Exception e) {
+            messageField.setText("An error has occurred: "+e);
+
+        }
+
+    }
+
+    public String getEnteredEmail() {
+        return this.email;
+    }
+
+    public String getEnteredPassword() {
+        return this.password;
     }
 
 }
